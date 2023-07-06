87a23cce4888cf74202548a4bf5bf2bfe2aed038
Muhamed Didovic <muhamed.didovic@gmail.com>
2023-07-06 17:15:04 +0200

    update gitignore, prettier add files for new version of nextjs

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
+  images: {
+    domains: ['firebasestorage.googleapis.com']
+  }
+}
+
+module.exports = nextConfig
